10f11bbfbf78e6e576af4b12412317d8f0c0e55b
themanmaran <tyler.maran@gmail.com>
2020-02-25 11:53:21 -0800

    Adding more topics to issues

--- server/mockData/issues.js
+++ server/mockData/issues.js
@@ -1,15 +1,23 @@
 const issues = [
   {
-    name: '',
-    joinDate: '6/14/19',
-    reputation: 19,
+    name:
+      'Translation updates do not work when I try to change the translations in the message.js file',
+    library: 'React-boilerplate',
+    link: 'https://github.com/react-boilerplate/react-boilerplate/issues/2699',
+    posted: '2/11/19',
+    overview:
+      'Translation updates do not work when I try to change the translations in the message.js file.',
     language: 'Javascript',
     languageLogo:
       'https://img.favpng.com/3/7/21/javascript-node-js-computer-icons-logo-application-software-png-favpng-ebFcx9xnNQyPYnT2RTy7PWBdV.jpg',
+    solved: false,
+    bids: 2,
+    setPrice: 60,
   },
   {
     name:
       'Decklink output only works "properly" on NV12/i420 with 601. All other have artefacts or 90%+ encoding lag',
+    library: 'OBS Studio',
     link: 'https://github.com/obsproject/obs-studio/issues/2398',
     posted: '2/11/19',
     overview:
@@ -18,10 +26,13 @@ const issues = [
     languageLogo:
       'https://i7.pngguru.com/preview/465/779/778/the-c-programming-language-computer-programming-computer-icons-programmer.jpg',
     solved: false,
+    bids: 7,
+    setPrice: 25,
   },
   {
     name:
       'Decklink output only works "properly" on NV12/i420 with 601. All other have artefacts or 90%+ encoding lag',
+    library: 'OBS Studio',
     link: 'https://github.com/obsproject/obs-studio/issues/2398',
     posted: '2/11/19',
     overview:
