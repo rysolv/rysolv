a1cd41866f4140c64987fadbf997d6ab62fab18b
themanmaran <tyler.maran@gmail.com>
2020-05-21 22:54:16 -0700

    prevent duplicate organizations under users

--- server/db/users.js
+++ server/db/users.js
@@ -157,13 +157,18 @@ const transformUser = async (table, id, data) => {
 };
 
 const updateUserArray = async (table, column, id, data, remove) => {
-  const action = remove ? 'array_remove' : 'array_append';
-  const queryText = `UPDATE ${table}
-    SET ${column} = ${action}(${column}, '${data}')
-    WHERE (id = '${id}')
-    RETURNING *`;
-  const { rows } = await singleQuery(queryText);
-  return rows;
+  const [userData] = await getOneUser('users', id, 'id');
+  // Only add uniquew values to array
+  if (!userData[column].includes(data)) {
+    const action = remove ? 'array_remove' : 'array_append';
+    const queryText = `UPDATE ${table}
+      SET ${column} = ${action}(${column}, '${data}')
+      WHERE (id = '${id}')
+      RETURNING *`;
+    const { rows } = await singleQuery(queryText);
+    return rows;
+  }
+  return false;
 };
 
 const userUpvote = async (table, id) => {
