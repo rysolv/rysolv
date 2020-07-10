b29561c5c0b9b47e3a3074e5516b57bbf658d440
Anna Pojawis <annapojawis@gmail.com>
2020-07-09 17:24:49 -0700

    Fix contact us page

--- app/components/Contact/index.jsx
+++ app/components/Contact/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import iconDictionary from 'utils/iconDictionary';
 
@@ -15,37 +15,43 @@ import {
 
 const EmailIcon = iconDictionary('email');
 
-const Contact = () => (
-  <ContactContainer>
-    <ContactHeader>Contact Us</ContactHeader>
-    <div>
-      <EmailContainer>
-        <IconWrapper>{EmailIcon}</IconWrapper>
-        <TopParagraph>
-          Don&apos;t hesitate to contact us if we can help with anything.
-          <br />
-          We&apos;d love to hear from you.
-        </TopParagraph>
-        <MiddleParagraph>
-          Please e-mail us at{' '}
-          <LinkWrapper href="mailto: support@rysolv.com">
-            support@rysolv.com
-          </LinkWrapper>{' '}
-          with questions or feedback.
-        </MiddleParagraph>
-        <BottomParagraph>
-          Found a bug? Let us know. Just{' '}
-          <LinkWrapper
-            href="https://github.com/rysolv/rysolv/issues"
-            target="_blank"
-          >
-            raise an issue
-          </LinkWrapper>
-          .
-        </BottomParagraph>
-      </EmailContainer>
-    </div>
-  </ContactContainer>
-);
+const Contact = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    document.title = 'Contact Us';
+  }, []);
+  return (
+    <ContactContainer>
+      <ContactHeader>Contact Us</ContactHeader>
+      <div>
+        <EmailContainer>
+          <IconWrapper>{EmailIcon}</IconWrapper>
+          <TopParagraph>
+            Don&apos;t hesitate to contact us if we can help with anything.
+            <br />
+            We&apos;d love to hear from you.
+          </TopParagraph>
+          <MiddleParagraph>
+            Please e-mail us at{' '}
+            <LinkWrapper href="mailto: support@rysolv.com">
+              support@rysolv.com
+            </LinkWrapper>{' '}
+            with questions or feedback.
+          </MiddleParagraph>
+          <BottomParagraph>
+            Found a bug? Let us know. Just{' '}
+            <LinkWrapper
+              href="https://github.com/rysolv/rysolv/issues"
+              target="_blank"
+            >
+              raise an issue
+            </LinkWrapper>
+            .
+          </BottomParagraph>
+        </EmailContainer>
+      </div>
+    </ContactContainer>
+  );
+};
 
 export default Contact;
