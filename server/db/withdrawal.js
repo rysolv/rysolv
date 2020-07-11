cbb8abe5ba91213e9389492abe146506939d5291
themanmaran <tyler.maran@gmail.com>
2020-07-11 14:50:43 -0700

    withdrawal cleanup

--- server/db/withdrawal.js
+++ server/db/withdrawal.js
@@ -1,24 +1,41 @@
-const { singleQuery } = require('../db/query');
+const { mapValues } = require('../db/query');
+const { formatParamaters } = require('./helpers');
+
+const withdrawalValues = [
+  'id',
+  'created_date',
+  'fee',
+  'transfer_value',
+  'user_id',
+];
 
 // CREATE single withdrawal
-const createWithdrawal = async (userId, withdrawalId, fee, transferValue) => {
-  const withdrawalQuery = `
-    INSERT INTO withdrawal(id, fee, transfer_value, user_id)
-    VALUES('${withdrawalId}', ${fee}, ${transferValue}, '${userId}')
-    RETURNING *`;
-  const { rows } = await singleQuery(withdrawalQuery);
-  return rows;
+const createWithdrawal = async data => {
+  const { parameters, substitution, values } = formatParamaters(
+    withdrawalValues,
+    data,
+  );
+
+  const queryText = `
+    INSERT INTO withdrawal(${parameters})
+    VALUES(${substitution})`;
+
+  await mapValues(queryText, values);
+  return 'Successfully created withdrawal';
 };
 
 // PATCH single user balance
-const transformUserBalance = async (userId, adjustedBalanceValue) => {
-  const userBalanceQuery = `
-    UPDATE users SET
-      balance = ${adjustedBalanceValue}
-    WHERE (id = '${userId}')
-    RETURNING *`;
-  const { rows } = await singleQuery(userBalanceQuery);
-  return rows;
+const transformUserBalance = async ({ adjustedBalanceValue, userId }) => {
+  const values = [[adjustedBalanceValue, userId]];
+
+  const queryText = `
+    UPDATE users
+    SET balance = $1
+    WHERE (id = $2)
+    RETURNING balance`;
+
+  const [result] = await mapValues(queryText, values);
+  return result;
 };
 
 module.exports = { createWithdrawal, transformUserBalance };
--- server/graphql/resolvers/withdrawalResolver.js
+++ server/graphql/resolvers/withdrawalResolver.js
@@ -9,30 +9,41 @@ const {
 module.exports = {
   createWithdrawal: async args => {
     const { transferValue, userId } = args;
+    console.log(transferValue);
     try {
-      const [{ balance }] = await getOneUser('users', userId);
+      const [{ balance }] = await getOneUser(userId);
       const lessThanBalance = balance >= transferValue;
       const greaterThanZero = transferValue > 0;
+
       if (lessThanBalance && greaterThanZero) {
+        // Update user balance
         const adjustedBalanceValue = balance - transferValue;
-        const fee = transferValue * 0.15;
-        const withdrawalId = uuidv4();
-        const [result] = await transformUserBalance(
+        const result = await transformUserBalance({
           userId,
           adjustedBalanceValue,
-        );
-        await createWithdrawal(userId, withdrawalId, fee, transferValue);
+        });
+
+        // Record new withdrawal
+        const withdrawal = {
+          created_date: new Date(),
+          fee: transferValue * 0.15,
+          id: uuidv4(),
+          transfer_value: transferValue,
+          user_id: userId,
+        };
+        await createWithdrawal(withdrawal);
+
         return {
           __typename: 'Withdrawal',
           ...result,
         };
       }
-      return {
-        __typename: 'Error',
-        message: lessThanBalance
+
+      throw new Error(
+        lessThanBalance
           ? 'Transfer amount is greater than balance.'
           : 'Transfer amount must be greater than $0.00.',
-      };
+      );
     } catch (err) {
       return {
         __typename: 'Error',
