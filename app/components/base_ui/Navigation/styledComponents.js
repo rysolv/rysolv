ad7b77a6c5592568102fd411d1adbe366fc1d758
Anna Pojawis <annapojawis@gmail.com>
2020-07-12 12:41:33 -0700

    Change hover color on StyledBackNav

--- app/components/base_ui/Navigation/styledComponents.js
+++ app/components/base_ui/Navigation/styledComponents.js
@@ -1,13 +1,16 @@
 import styled from 'styled-components';
-import { defaultFontSize } from 'defaultStyleHelper';
+
+import { defaultFontSize, textColor } from 'defaultStyleHelper';
 
 export const StyledBackNav = styled.a`
+  align-self: flex-start;
   color: grey;
   display: inline-flex;
-  align-self: flex-start;
-  margin: 1rem 0;
   font-size: ${defaultFontSize};
+  margin: 1rem 0;
+
   &:hover {
+    color: ${textColor};
     cursor: pointer;
   }
 `;
