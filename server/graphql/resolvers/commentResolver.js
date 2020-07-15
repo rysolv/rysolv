ca6640300370501003398ce2505f225f0030ef1d
Anna Pojawis <annapojawis@gmail.com>
2020-07-15 13:55:07 -0700

    Fix updateIssueArray in createComment resolver as it causes comments to error when user posted a new one

--- server/graphql/resolvers/commentResolver.js
+++ server/graphql/resolvers/commentResolver.js
@@ -12,45 +12,47 @@ const {
 module.exports = {
   createComment: async args => {
     const { commentInput } = args;
-    const comment = [
-      [
-        uuidv4(),
-        new Date(),
-        new Date(),
-        commentInput.target,
-        commentInput.body || '',
+    try {
+      const comment = [
+        [
+          uuidv4(),
+          new Date(),
+          new Date(),
+          commentInput.target,
+          commentInput.body || '',
+          commentInput.user,
+        ],
+      ];
+      const [result] = await createComment(comment);
+
+      const activityInput = {
+        actionType: 'comment',
+        issueId: result.target,
+        userId: result.user_id,
+      };
+      await createActivity({ activityInput });
+
+      const [user] = await updateUserArray(
+        'users',
+        'comments',
         commentInput.user,
-      ],
-    ];
-    const [result] = await createComment(comment);
+        result.id,
+      );
+      await updateIssueArray('comments', result.target, result.id);
 
-    const activityInput = {
-      actionType: 'comment',
-      issueId: result.target,
-      userId: result.user_id,
-    };
-
-    await createActivity({ activityInput });
-
-    const [user] = await updateUserArray(
-      'users',
-      'comments',
-      commentInput.user,
-      result.id,
-    );
-
-    await updateIssueArray('issues', 'comments', result.target, result.id);
-
-    return {
-      commentId: result.id,
-      createdDate: result.created_date,
-      modifiedDate: result.modified_date,
-      target: result.target,
-      body: result.body,
-      userId: result.user_id,
-      username: user.username,
-      profilePic: user.profile_pic,
-    };
+      return {
+        commentId: result.id,
+        createdDate: result.created_date,
+        modifiedDate: result.modified_date,
+        target: result.target,
+        body: result.body,
+        userId: result.user_id,
+        username: user.username,
+        profilePic: user.profile_pic,
+      };
+    } catch (err) {
+      throw err;
+    }
   },
   getComments: async () => {
     try {
