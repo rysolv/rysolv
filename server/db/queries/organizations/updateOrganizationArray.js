64d381f06e22f6fb705e02b344d101e5e55b209a
Anna Pojawis <annapojawis@gmail.com>
2020-09-02 21:10:48 -0700

    Alpha

--- server/db/queries/organizations/updateOrganizationArray.js
+++ server/db/queries/organizations/updateOrganizationArray.js
@@ -2,7 +2,7 @@ const { singleQuery } = require('../../baseQueries');
 
 // @TODO: remove this query entirely. Refactor getOrganizations to join issues
 // ADD to organization array
-const updateOrganizationArray = async ({ column, id, data, remove }) => {
+const updateOrganizationArray = async ({ column, data, id, remove }) => {
   const action = remove ? 'array_remove' : 'array_append';
   const queryText = `UPDATE organizations
     SET ${column} = ${action}(${column}, '${data}')
