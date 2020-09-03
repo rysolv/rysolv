1c4e24f1e546a0aff461b5856a12493d6621e348
Anna Pojawis <annapojawis@gmail.com>
2020-09-02 18:54:25 -0700

    Remove diff function

--- server/db/helpers/index.js
+++ server/db/helpers/index.js
@@ -1,25 +1,3 @@
-/* eslint-disable no-console */
-const { isNull, isUndefined } = require('lodash');
-
-const isDefined = value => !(isNull(value) || isUndefined(value));
-
-// Compare two objects and create a new object with the changes from obj2
-//    obj1={first: "Taylor", last: "Maran"};
-//    obj2={name: "Tyler"};
-//    result = {first: "Tyler", last: "Maran"}
-const diff = (obj1, obj2) => {
-  const newObject = { ...obj1 };
-  const newObjectArray = [];
-  const compare = (item1, item2, key) => {
-    if (isDefined(item2) && item2 !== item1) {
-      newObject[key] = item2;
-    }
-    newObjectArray.push(newObject[key]);
-  };
-  Object.keys(obj1).map(key => compare(obj1[key], obj2[key], key));
-  return { newObject, newObjectArray };
-};
-
 // Format Parameters for node-pg parameterized queries. Returning:
 //    parameters: array of parameters (ex: "id, created_date, modified_date")
 //    substitution: string (ex: "$1, $2, $3")
@@ -44,4 +22,4 @@ const formatParameters = ({ newObject, tableParameters }) => {
   };
 };
 
-module.exports = { diff, formatParameters };
+module.exports = { formatParameters };
