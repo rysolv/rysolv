1f8eb73b76e7d9f74a095605cace7cc743264a7a
Anna Pojawis <annapojawis@gmail.com>
2020-09-02 19:12:53 -0700

    Add space

--- server/db/queries/issues/checkDuplicateIssue.js
+++ server/db/queries/issues/checkDuplicateIssue.js
@@ -2,7 +2,7 @@ const { singleQuery } = require('../../baseQueries');
 
 // Check duplicate issue
 const checkDuplicateIssue = async ({ repo }) => {
-  const queryText = `SELECT id FROM issues WHERE repo= $1`;
+  const queryText = `SELECT id FROM issues WHERE repo = $1`;
   const { rows } = await singleQuery({ queryText, values: [repo] });
   return rows.length > 0;
 };
