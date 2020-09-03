6d7ff850561ec72e3c2e2e98c322586f7d1e04dd
Anna Pojawis <annapojawis@gmail.com>
2020-09-02 18:10:44 -0700

    Remove more things from users container

--- app/containers/Users/constants.js
+++ app/containers/Users/constants.js
@@ -1,14 +1,6 @@
 export const CHANGE_USER_FILTER = 'app/Users/CHANGE_USER_FILTER';
 export const CHANGE_USER_SEARCH = 'app/Users/CHANGE_USER/SEARCH';
 
-export const CLEAR_ALERTS = 'app/Users/CLEAR_ALERTS';
-
-export const CLEAR_FORM = 'app/Users/CLEAR_FORM';
-
-export const DELETE_USER = 'app/Users/DELETE_USER';
-export const DELETE_USER_FAILURE = 'app/Users/DELETE_USER_FAILURE';
-export const DELETE_USER_SUCCESS = 'app/Users/DELETE_USER_SUCCESS';
-
 export const FETCH_INFO = 'app/Users/FETCH_INFO';
 export const FETCH_INFO_FAILURE = 'app/Users/FETCH_INFO_FAILURE';
 export const FETCH_INFO_SUCCESS = 'app/Users/FETCH_INFO_SUCCESS';
@@ -17,21 +9,11 @@ export const FETCH_USERS = 'app/Users/FETCH_USERS';
 export const FETCH_USERS_FAILURE = 'app/Users/FETCH_USERS_FAILURE';
 export const FETCH_USERS_SUCCESS = 'app/Users/FETCH_USERS_SUCCESS';
 
-export const INCREMENT_STEP = 'app/Users/INCREMENT_STEP';
-
 export const INPUT_CHANGE = 'app/Users/INPUT_CHANGE';
-
-export const SAVE_INFO = 'app/Users/SAVE_INFO';
-export const SAVE_INFO_FAILURE = 'app/Users/SAVE_INFO_FAILURE';
-export const SAVE_INFO_SUCCESS = 'app/Users/SAVE_INFO_SUCCESS';
 
 export const SEARCH_USERS = 'app/Users/SEARCH_USERS';
 export const SEARCH_USERS_FAILURE = 'app/Users/SEARCH_USERS_FAILURE';
 export const SEARCH_USERS_SUCCESS = 'app/Users/SEARCH_USERS_SUCCESS';
-
-export const UPDATE_INFO = 'app/Users/UPDATE_INFO';
-export const UPDATE_INFO_FAILURE = 'app/Users/UPDATE_INFO_FAILURE';
-export const UPDATE_INFO_SUCCESS = 'app/Users/UPDATE_INFO_SUCCESS';
 
 export const actionDictionary = {
   Commented: 'commented on',
