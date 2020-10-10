0a3605fd7fe654384b47c0dec20b36418b7b8378
Anna Pojawis <annapojawis@gmail.com>
2020-10-09 21:23:30 -0400

    Undo style changes

--- app/components/Faq/styledComponent.js
+++ app/components/Faq/styledComponent.js
@@ -16,7 +16,7 @@ export const AnswerWrapper = styled.div`
   display: flex;
   font-size: ${defaultFontSize};
   line-height: 1.5;
-  padding: 0 2.5rem 2.5rem 0;
+  padding-bottom: 2.5rem;
   text-align: start;
 `;
 
