dcd14bd1d494d5c9bd12e7107173a884a711e126
Anna Pojawis <annapojawis@gmail.com>
2020-10-10 02:33:16 -0400

    Fix for unknown user

--- app/components/Organizations/Detail/RecentActivityView.jsx
+++ app/components/Organizations/Detail/RecentActivityView.jsx
@@ -6,6 +6,7 @@ import T from 'prop-types';
 import { ConditionalRender, ImageLinkWrapper } from 'components/base_ui';
 import { formatDollarAmount } from 'utils/globalHelpers';
 
+import { anonymousUserImage } from './constants';
 import {
   ActivityContainer,
   ActivityDate,
@@ -34,34 +35,43 @@ export class RecentActivityView extends React.PureComponent {
             path,
             target: { targetName, targetType },
             user: { userId, username, profilePic },
-          }) => (
-            <ActivityWrapper key={activityId}>
-              <div style={{ display: 'flex' }}>
-                <ProfileImageWrapper>
-                  <ImageLinkWrapper
-                    alt={username}
-                    image={profilePic}
-                    route={`/users/detail/${userId}`}
-                  />
-                </ProfileImageWrapper>
-                <FundContent>
-                  <StyledWordLink to={`/users/detail/${userId}`}>
-                    {username}
-                  </StyledWordLink>
-                  &nbsp;
-                  <StyledAction>
-                    {action} {targetType.toLowerCase()}
-                  </StyledAction>
-                  &nbsp;
-                  {fundedValue ? `for ${formatDollarAmount(fundedValue)} ` : ''}
-                  <StyledExternalLink to={path}>
-                    {targetName}
-                  </StyledExternalLink>
-                  <ActivityDate>{moment(date).fromNow()}</ActivityDate>
-                </FundContent>
-              </div>
-            </ActivityWrapper>
-          ),
+          }) => {
+            const disabled = !userId;
+            return (
+              <ActivityWrapper key={activityId}>
+                <div style={{ display: 'flex' }}>
+                  <ProfileImageWrapper>
+                    <ImageLinkWrapper
+                      alt={username || 'anonymous'}
+                      disabled={disabled}
+                      image={profilePic || anonymousUserImage}
+                      route={`/users/detail/${userId}`}
+                    />
+                  </ProfileImageWrapper>
+                  <FundContent>
+                    <StyledWordLink
+                      disabled={disabled}
+                      to={`/users/detail/${userId}`}
+                    >
+                      {username || 'anonymous'}
+                    </StyledWordLink>
+                    &nbsp;
+                    <StyledAction>
+                      {action} {targetType.toLowerCase()}
+                    </StyledAction>
+                    &nbsp;
+                    {fundedValue
+                      ? `for ${formatDollarAmount(fundedValue)} `
+                      : ''}
+                    <StyledExternalLink to={path}>
+                      {targetName}
+                    </StyledExternalLink>
+                    <ActivityDate>{moment(date).fromNow()}</ActivityDate>
+                  </FundContent>
+                </div>
+              </ActivityWrapper>
+            );
+          },
         )}
       </ActivityContainer>
     );
--- app/components/Organizations/Detail/constants.js
+++ app/components/Organizations/Detail/constants.js
@@ -1,3 +1,6 @@
+export const anonymousUserImage =
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTBvwAcytGFLkWO2eT-FCwE5z_mlQxBdI9uwbyeczCTVBci7Vrg&usqp=CAU';
+
 export const blueColorSpectrum = [
   '#1565c0',
   '#1976d2',
