367284d31c4b179b466bde8fae9e5ff20e37a40e
Anna Pojawis <annapojawis@gmail.com>
2020-10-11 21:02:42 -0400

    Different url for anonymousUserImage

--- app/components/base_ui/ImageLinkWrapper/constants.js
+++ app/components/base_ui/ImageLinkWrapper/constants.js
@@ -1,2 +1,2 @@
 export const anonymousUserImage =
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTBvwAcytGFLkWO2eT-FCwE5z_mlQxBdI9uwbyeczCTVBci7Vrg&usqp=CAU';
+  'https://rysolv.s3.us-east-2.amazonaws.com/anonymous.png';
