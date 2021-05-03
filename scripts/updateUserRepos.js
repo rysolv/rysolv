d2e5f1553f160e3e9f9e7a3d62288df7d7b32a70
themanmaran <tyler.maran@gmail.com>
2021-05-03 18:48:33 -0400

    tm updates

--- scripts/updateUserRepos.js
+++ scripts/updateUserRepos.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console, no-unused-vars */
 const { Octokit } = require('@octokit/rest');
 const { createTokenAuth } = require('@octokit/auth-token');
+const { v4: uuidv4 } = require('uuid');
 
 const { connect } = require('./connect');
 const { formatRepoUrl } = require('../server/integrations/github/helpers');
@@ -10,8 +11,8 @@ const env = process.argv[2];
 const { pool, singleQuery } = connect(env);
 
 /**
- * Update existing repos with the github_owner, and check if any repos have a
- * github_owner that is a Rysolv user
+ * Update existing repos with the github_owner and rysolv_owner
+ * Check if any repos have a github_owner that is a Rysolv user
  */
 async function updateUserRepos() {
   const t1 = Date.now();
@@ -24,16 +25,9 @@ async function updateUserRepos() {
   });
 
   const getReposQuery = `
-    SELECT
-      repos.repo_url AS "repoUrl",
-      user_repos.id
-    FROM repos
-    LEFT JOIN user_repos ON user_repos.repo_id = repos.id
-    WHERE repos.is_deleted = false
-    AND repos.is_edited = false
-    AND repos.is_manual = false
-    AND user_repos.user_type = 'github_owner'
-    AND user_repos.github_id IS NULL
+    SELECT r.owner_id AS "userId", r.id AS "repoId", r.repo_url AS "repoUrl" FROM repos r
+    LEFT JOIN user_repos ur ON ur.repo_id = r.id
+    WHERE ur.id IS null
   `;
 
   // Get repos
@@ -42,7 +36,7 @@ async function updateUserRepos() {
   );
   console.log(`Updating ${rows.length} repos`);
   await Promise.all(
-    rows.map(async ({ id, repoUrl }) => {
+    rows.map(async ({ userId, repoId, repoUrl }) => {
       const { organization, repo } = formatRepoUrl(repoUrl);
 
       const {
@@ -52,13 +46,22 @@ async function updateUserRepos() {
         repo,
       });
 
+      console.log(owner.login);
+      const values = [uuidv4(), owner.id, repoId, userId];
+
       const updateUserReposQuery = `
-        UPDATE user_repos SET github_id = $1
-        WHERE id = $2
+        INSERT INTO user_repos (id, github_id, repo_id, user_id, user_type)
+        SELECT $1, $2, $3, $4, 'github_owner'
+        WHERE NOT EXISTS (
+          SELECT 1 FROM user_repos ur
+          WHERE ur.user_id = $4
+          AND ur.repo_id =  $3
+          AND ur.user_type = 'github_owner'
+        )
       `;
       await singleQuery({
         queryText: updateUserReposQuery,
-        values: [owner.id, id],
+        values,
       });
     }),
   );
@@ -80,7 +83,7 @@ async function updateUserRepos() {
   const { rows: githubOwnerRows } = await singleQuery({
     queryText: getGithubOwnerQuery,
   }).catch(err => console.log('Get github owners: ', err));
-  console.log(`Updating ${rows.length} github owners`);
+  console.log(`Updating ${githubOwnerRows.length} github owners`);
   await Promise.all(
     githubOwnerRows.map(async ({ githubId, id }) => {
       const getUserIdQuery = `
@@ -101,6 +104,47 @@ async function updateUserRepos() {
       });
     }),
   );
+
+  // Add rysolvOwner to user_repos
+  const getRysolvOwnerQuery = `
+    SELECT
+      r.id AS "repoId",
+      r.owner_id AS "userId",
+      u.github_id AS "githubId"
+    FROM repos r
+    JOIN users u ON u.id = r.owner_id
+  `;
+
+  // Get github owners
+  const { rows: rysolvOwners } = await singleQuery({
+    queryText: getRysolvOwnerQuery,
+  }).catch(err => console.log('Get rysolv owners: ', err));
+
+  console.log(`Updating ${rysolvOwners.length} rysolv owners`);
+
+  let rowsInserted = 0;
+  await Promise.all(
+    rysolvOwners.map(async ({ githubId, repoId, userId }) => {
+      const values = [uuidv4(), githubId, repoId, userId];
+
+      const insertRysolvOwnerQuery = `
+        INSERT INTO user_repos (id, github_id, repo_id, user_id, user_type)
+        SELECT $1, $2, $3, $4, 'rysolv_owner'
+        WHERE NOT EXISTS (
+          SELECT 1 FROM user_repos ur
+          WHERE ur.user_id = $4
+          AND ur.repo_id =  $3
+          AND ur.user_type = 'rysolv_owner'
+        )
+      `;
+      const { rowCount } = await singleQuery({
+        queryText: insertRysolvOwnerQuery,
+        values,
+      });
+      rowsInserted += rowCount;
+    }),
+  );
+  console.log(`Added ${rowsInserted} rysolv owners`);
 
   const t2 = Date.now();
   pool.end();
