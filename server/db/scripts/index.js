a2851210c44730cdbbc0368e531e098c8a0d558d
Anna Pojawis <annapojawis@gmail.com>
2021-05-21 23:40:15 -0400

    Remove script

--- server/db/scripts/index.js
+++ server/db/scripts/index.js
@@ -1,6 +1,5 @@
 const pool = require('../connect');
 const { seedQuestions, seedResponses } = require('./questions');
-const { seedUserType } = require('./userType');
 
 // Populate the Q&A data for jobs
 const generateQuestions = async () => {
@@ -10,16 +9,8 @@ const generateQuestions = async () => {
   return 'Finished running generateQuestions';
 };
 
-// Populate the user_type column in users
-const generateUserType = async () => {
-  await seedUserType();
-  pool.end();
-  return 'Finished running generateUserType';
-};
-
 module.exports = {
   generateQuestions,
-  generateUserType,
 };
 
 require('make-runnable/custom')({
