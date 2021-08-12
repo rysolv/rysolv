3d8fab76602dee3f8192ce444be1a243ffb9019d
themanmaran <tyler.maran@gmail.com>
2021-08-11 22:17:02 -0400

    hotfix, recording github emails on sign-in, and adding unique clause to email

--- migrations/sqls/20210806015938-create-user-emails-table-up.sql
+++ migrations/sqls/20210806015938-create-user-emails-table-up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS user_emails (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    email VARCHAR(256),
+    email VARCHAR(256) UNIQUE,
     primary_email BOOLEAN,
     user_id UUID REFERENCES users(id)
 );
