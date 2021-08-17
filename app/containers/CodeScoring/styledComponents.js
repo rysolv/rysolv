390423cfd68aab3e2d30724bd6567ca51d71e1d2
themanmaran <tyler.maran@gmail.com>
2021-08-16 21:28:45 -0400

    center code scoring page/

--- app/containers/CodeScoring/styledComponents.js
+++ app/containers/CodeScoring/styledComponents.js
@@ -4,6 +4,8 @@ import { blueColor } from 'defaultStyleHelper';
 
 export const ViewContainer = styled.div`
   background: ${blueColor};
+  display: flex;
+  justify-content: center;
   margin-bottom: 5.6rem;
   width: 100%;
 `;
