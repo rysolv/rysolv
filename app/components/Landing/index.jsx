012f916be34b3ec933231639b4f96ecad4ee5ce3
Anna Pojawis <annapojawis@gmail.com>
2021-08-22 18:12:07 -0400

    Update slogan to hiring

--- app/components/Landing/index.jsx
+++ app/components/Landing/index.jsx
@@ -75,7 +75,7 @@ const Landing = ({
       <div>
         <LandingWrapper>
           <LandingContainerText>
-            A crowdfunding <br />platform for <br /> <span>open source</span>.
+            A platform<br/> where <span>engineers</span><br />come to grow.
           </LandingContainerText>
           <ButtonGroup>
             <StyledSecondaryButton
