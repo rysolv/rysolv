7880eedda76f6624ced98067d3280ad046be1a63
themanmaran <tyler.maran@gmail.com>
2022-02-03 00:26:02 -0500

    removed saga, reducer, etc. from landing page

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -9,29 +9,16 @@ import { Redirect, withRouter } from 'react-router-dom';
 import { ConditionalRender } from 'components/base_ui';
 import Landing from 'components/Landing';
 import { makeSelectAuth } from 'containers/Auth/selectors';
-import injectReducer from 'utils/injectReducer';
-import injectSaga from 'utils/injectSaga';
 
-import { fetchHomePageStats, resetFeedback, sendContact } from './actions';
-import reducer from './reducer';
-import saga from './saga';
-import { makeSelectHomePage } from './selectors';
 import { HomePageContainer } from './styledComponents';
 
 const HomePage = ({
   activeUser,
-  dispatchFetchHomePageStats,
-  dispatchResetFeedback,
-  dispatchSendContact,
-  error,
+
   handleNav,
   isSignedIn,
-  loading,
-  stats,
-  success,
 }) => {
   useEffect(() => {
-    dispatchFetchHomePageStats();
     document.title = 'Rysolv';
   }, []);
 
@@ -40,15 +27,7 @@ const HomePage = ({
 
   const HomePageComponent = (
     <HomePageContainer>
-      <Landing
-        dispatchResetFeedback={dispatchResetFeedback}
-        dispatchSendContact={dispatchSendContact}
-        error={error}
-        handleNav={handleNav}
-        loading={loading}
-        stats={stats}
-        success={success}
-      />
+      <Landing handleNav={handleNav} />
     </HomePageContainer>
   );
 
@@ -63,15 +42,8 @@ const HomePage = ({
 
 HomePage.propTypes = {
   activeUser: T.object.isRequired,
-  dispatchFetchHomePageStats: T.func.isRequired,
-  dispatchResetFeedback: T.func.isRequired,
-  dispatchSendContact: T.func.isRequired,
-  error: T.bool.isRequired,
   handleNav: T.func.isRequired,
   isSignedIn: T.bool.isRequired,
-  loading: T.bool.isRequired,
-  stats: T.object.isRequired,
-  success: T.bool.isRequired,
 };
 
 const mapStateToProps = createStructuredSelector({
@@ -80,22 +52,9 @@ const mapStateToProps = createStructuredSelector({
    */
   activeUser: makeSelectAuth('activeUser'),
   isSignedIn: makeSelectAuth('isSignedIn'),
-  /*
-   * Reducer : HomePage
-   */
-  error: makeSelectHomePage('error'),
-  loading: makeSelectHomePage('loading'),
-  stats: makeSelectHomePage('stats'),
-  success: makeSelectHomePage('success'),
 });
 
 const mapDispatchToProps = dispatch => ({
-  /*
-   * Reducer : HomePage
-   */
-  dispatchFetchHomePageStats: () => dispatch(fetchHomePageStats()),
-  dispatchResetFeedback: () => dispatch(resetFeedback()),
-  dispatchSendContact: payload => dispatch(sendContact(payload)),
   /*
    * Reducer : Router
    */
@@ -106,13 +65,5 @@ const withConnect = connect(
   mapStateToProps,
   mapDispatchToProps,
 );
-const withReducer = injectReducer({ key: 'homePage', reducer });
-const withSaga = injectSaga({ key: 'homePage', saga });
 
-export default withRouter(
-  compose(
-    withReducer,
-    withSaga,
-    withConnect,
-  )(HomePage),
-);
+export default withRouter(compose(withConnect)(HomePage));
